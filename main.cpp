9c157db9d8c0cb100aa6aed0f244892b536238ac
tawfeh <sennegueye@gmail.com>
2017-08-07 15:36:08 +0000

    Add all files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+       for (int compteur(0) ; compteur < 10 ; compteur++)
+            {
+                  cout << compteur << endl;
+                   }
+       return 0;
+       }
